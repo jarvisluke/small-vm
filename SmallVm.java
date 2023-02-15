35e96fc4afbc38338ecd0899976857d6b9bc88cd
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-02-14 22:29:28 -0500

    Update SmallVm.java
    Edited comments, created printGreeting method

--- SmallVm.java
+++ SmallVm.java
@@ -1,3 +1,9 @@
+/*
+Author: Luke Jarvis
+Date: 2/14/2023
+Class: CSCI 4200
+Instructor: Abi Salimi
+ */
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,12 +12,16 @@ import java.util.HashMap;
 public class SmallVm {
 
     private final static int MAX_MEMORY_SIZE = 500;
-    private String[] memory = new String[MAX_MEMORY_SIZE];
-    private HashMap<String, String> vars = new HashMap<>(); // Stores variables
+    private String[] memory = new String[MAX_MEMORY_SIZE]; // Stores instructions
+    private HashMap<String, String> heap = new HashMap<>(); // Stores variables
     int programCounter = 0; // Tracks where in the memory the instruction is
 
+    public SmallVm(File file) {
+        loadToMemory(file);
+    }
+
     // Loads the program instructions into memory
-    public void loadToMemory(File file) {
+    void loadToMemory(File file) {
         try {
             Scanner scan = new Scanner(file);
             int loader = 0; // Keeps track of where in memory the instructions are being loaded
@@ -23,11 +33,13 @@ public class SmallVm {
                         loader++;
                     }
                 }
-            } catch (ArrayIndexOutOfBoundsException e) { // Exception if memory capacity is exceeded
+            }
+            catch (ArrayIndexOutOfBoundsException e) { // Exception if memory capacity is exceeded
                 System.out.println("Error: program exceeds memory capacity; " + e);
             }
             scan.close();
-        } catch (FileNotFoundException e) { // Exception if file is not located
+        }
+        catch (FileNotFoundException e) { // Exception if file is not located
             e.printStackTrace();
         }
     }
@@ -35,39 +47,39 @@ public class SmallVm {
     // Stores the result of adding num1 and num2 as name
     void ADD(String name, int num1, int num2) {
         int result = num1 + num2;
-        vars.put(name, String.valueOf(result));
+        heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of subtracting from num1 num2 as name
     void SUB(String name, int num1, int num2) {
         int result = num1 - num2;
-        vars.put(name, String.valueOf(result));
+        heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of multiplying num1 by num2 as name
     void MUL(String name, int num1, int num2) {
         int result = num1 * num2;
-        vars.put(name, String.valueOf(result));
+        heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of diving num1 by num2 as name
     void DIV(String name, int num1, int num2) {
         int result = num1 / num2;
-        vars.put(name, String.valueOf(result));
+        heap.put(name, String.valueOf(result));
     }
 
     // Stores a variable from num
     void STO(String name, String num) {
-        vars.put(name, num);
+        heap.put(name, num);
     }
 
     // Stores a variable from the terminal
     void IN(String name) {
         Scanner scan = new Scanner(System.in);
-        vars.put(name, scan.next());
+        heap.put(name, scan.next());
     }
 
-    // Outputs the output to the terminal
+    // Outputs output to the terminal
     void OUT(String output) {
         System.out.println(output);
     }
@@ -77,28 +89,28 @@ public class SmallVm {
         return memory[programCounter];
     }
 
-    // Evaluations variables where an int is expected as the argument
+    // Evaluations variables to ints
     int evaluateArg(String arg) {
         try {
             return Integer.parseInt(arg);
         }
         catch (NumberFormatException e) {
-            return Integer.parseInt(vars.get(arg));
+            return Integer.parseInt(heap.get(arg));
         }
     }
 
-    // Evaluates variables where a string is expected as the argument
+    // Evaluates variables to strings
     String evaluateRegular(String arg) {
         arg = arg.trim();
-        if (vars.containsKey(arg)) {
-            return vars.get(arg);
+        if (heap.containsKey(arg)) {
+            return heap.get(arg);
         }
         else return arg.substring(1, arg.length()-1);
     }
 
     // Decodes instruction and calls operation
     void execute(String instruction) {
-        Scanner scan = new Scanner(instruction);
+        Scanner scan = new Scanner(instruction); // Nathan Vrieland suggested using the scanner to pass arguments
         switch (scan.next()) {
             case "ADD" -> ADD(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
             case "SUB" -> SUB(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
@@ -108,15 +120,38 @@ public class SmallVm {
             case "IN" -> IN(scan.next());
             case "OUT" -> OUT(evaluateRegular(scan.nextLine()));
             case "HALT" -> System.exit(0);
-            default -> System.exit(1);
+            default -> System.exit(1); // Operation not recognized
         }
         scan.close();
     }
 
+    // Prints the required display
+    static void printGreeting(File file) {
+        System.out.println("Luke Jarvis, CSCI 4200 (Spring 2023)");
+        for (int i = 0; i < 42; i ++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        try {
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+            scan.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 42; i ++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         File file = new File("mySmallVm_Prog.txt");
-        SmallVm myVm = new SmallVm();
-        myVm.loadToMemory(file);
+        printGreeting(file);
+        SmallVm myVm = new SmallVm(file);
         while (true) {
             String instruction = myVm.fetch();
             myVm.programCounter++;
