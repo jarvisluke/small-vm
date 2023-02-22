d392cbae2662b7c5856014ec06c2ed21355067fb
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-02-22 18:26:13 -0500

    Update SmallVm.java
    Added run command, renamed SmallVm to vm

--- SmallVm.java
+++ SmallVm.java
@@ -12,81 +12,85 @@ import java.util.HashMap;
 public class SmallVm {
 
     private final static int MAX_MEMORY_SIZE = 500;
-    private String[] memory = new String[MAX_MEMORY_SIZE]; // Stores instructions
-    private HashMap<String, String> heap = new HashMap<>(); // Stores variables
-    int programCounter = 0; // Tracks where in the memory the instruction is
+    private String[] memory = new String[MAX_MEMORY_SIZE];
+    private HashMap<String, String> heap = new HashMap<>();
 
     public SmallVm(File file) {
         loadToMemory(file);
     }
 
-    // Loads the program instructions into memory
-    void loadToMemory(File file) {
+    // Loads the program into memory
+    private void loadToMemory(File file) {
         try {
             Scanner scan = new Scanner(file);
-            int loader = 0; // Keeps track of where in memory the instructions are being loaded
+            int i = 0;
             try {
                 while (scan.hasNextLine()) {
                     String nextLine = scan.nextLine();
-                    if (nextLine.charAt(0) != ';') { // Checks if the line is a comment
-                        memory[loader] = nextLine;
-                        loader++;
+                    // Removes any comments from nextLine
+                    int endPoint = nextLine.length();
+                    for (int j = 0; j < endPoint; j++) {
+                        if (nextLine.charAt(j) == ';') {
+                            endPoint = j;
+                        }
+                    }
+                    nextLine = nextLine.substring(0, endPoint);
+                    // Adds nextLine to memory if nextLine is not empty (i.e., if it does not begin with a comment symbol)
+                    if (nextLine.length() > 0) {
+                        memory[i] = nextLine;
+                        i++;
                     }
                 }
             }
-            catch (ArrayIndexOutOfBoundsException e) { // Exception if memory capacity is exceeded
+            // Exception if memory capacity is exceeded
+            catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Error: program exceeds memory capacity; " + e);
             }
             scan.close();
         }
-        catch (FileNotFoundException e) { // Exception if file is not located
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     // Stores the result of adding num1 and num2 as name
-    void ADD(String name, int num1, int num2) {
+    private void add(String name, int num1, int num2) {
         int result = num1 + num2;
         heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of subtracting from num1 num2 as name
-    void SUB(String name, int num1, int num2) {
+    private void sub(String name, int num1, int num2) {
         int result = num1 - num2;
         heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of multiplying num1 by num2 as name
-    void MUL(String name, int num1, int num2) {
+    private void mul(String name, int num1, int num2) {
         int result = num1 * num2;
         heap.put(name, String.valueOf(result));
     }
 
     // Stores the result of diving num1 by num2 as name
-    void DIV(String name, int num1, int num2) {
+    private void div(String name, int num1, int num2) {
         int result = num1 / num2;
         heap.put(name, String.valueOf(result));
     }
 
     // Stores a variable from num
-    void STO(String name, String num) {
+    private void sto(String name, String num) {
         heap.put(name, num);
     }
 
     // Stores a variable from the terminal
-    void IN(String name) {
+    private void in(String name) {
         Scanner scan = new Scanner(System.in);
         heap.put(name, scan.next());
     }
 
     // Outputs output to the terminal
-    void OUT(String output) {
+    private void out(String output) {
         System.out.println(output);
-    }
-
-    // Returns the instruction at the program counter in memory
-    String fetch() {
-        return memory[programCounter];
     }
 
     // Evaluations variables to ints
@@ -94,6 +98,7 @@ public class SmallVm {
         try {
             return Integer.parseInt(arg);
         }
+        // Exception occurs when arg cannot be parsed to an int, which is when arg is a variable
         catch (NumberFormatException e) {
             return Integer.parseInt(heap.get(arg));
         }
@@ -102,9 +107,7 @@ public class SmallVm {
     // Evaluates variables to strings
     String evaluateRegular(String arg) {
         arg = arg.trim();
-        if (heap.containsKey(arg)) {
-            return heap.get(arg);
-        }
+        if (heap.containsKey(arg)) return heap.get(arg);
         else return arg.substring(1, arg.length()-1);
     }
 
@@ -112,26 +115,32 @@ public class SmallVm {
     void execute(String instruction) {
         Scanner scan = new Scanner(instruction); // Nathan Vrieland suggested using the scanner to pass arguments
         switch (scan.next()) {
-            case "ADD" -> ADD(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
-            case "SUB" -> SUB(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
-            case "MUL" -> MUL(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
-            case "DIV" -> DIV(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
-            case "STO" -> STO(scan.next(), scan.next());
-            case "IN" -> IN(scan.next());
-            case "OUT" -> OUT(evaluateRegular(scan.nextLine()));
+            case "ADD" -> add(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
+            case "SUB" -> sub(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
+            case "MUL" -> mul(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
+            case "DIV" -> div(scan.next(), evaluateArg(scan.next()), evaluateArg(scan.next()));
+            case "STO" -> sto(scan.next(), scan.next());
+            case "IN" -> in(scan.next());
+            case "OUT" -> out(evaluateRegular(scan.nextLine()));
             case "HALT" -> System.exit(0);
-            default -> System.exit(1); // Operation not recognized
+            default -> System.exit(-1); // Operation not recognized
         }
         scan.close();
     }
 
+    public void run() {
+        int programCounter = 0;
+        while (true) {
+            String instruction = memory[programCounter];
+            programCounter++;
+            execute(instruction);
+        }
+    }
+
     // Prints the required display
     static void printGreeting(File file) {
-        System.out.println("Luke Jarvis, CSCI 4200 (Spring 2023)");
-        for (int i = 0; i < 42; i ++) {
-            System.out.print("*");
-        }
-        System.out.println();
+        System.out.println("Luke Jarvis\nCSCI 4200, Spring 2023");
+        System.out.println("**********************************************");
         try {
             Scanner scan = new Scanner(file);
             while(scan.hasNextLine()) {
@@ -140,22 +149,14 @@ public class SmallVm {
             scan.close();
         }
         catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < 42; i ++) {
-            System.out.print("*");
-        }
-        System.out.println();
+            e.printStackTrace(); }
+        System.out.println("**********************************************");
     }
 
     public static void main(String[] args) {
         File file = new File("mySmallVm_Prog.txt");
         printGreeting(file);
-        SmallVm myVm = new SmallVm(file);
-        while (true) {
-            String instruction = myVm.fetch();
-            myVm.programCounter++;
-            myVm.execute(instruction);
-        }
+        SmallVm vm = new SmallVm(file);
+        vm.run();
     }
 }
