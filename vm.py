62b1ffaef1b7c03042d57f11fdd619f533ab9e58
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-02-27 21:48:32 -0500

    Update vm.py

--- vm.py
+++ vm.py
@@ -65,8 +65,17 @@ class vm:
             for line in f.readlines():
                 # checks if line is a comment
                 if ';' not in line:
+                    # removes \n from line if present
+                    if line[-1] == '\n':
+                        self._memory.append(line.replace('\n', ''))
+                    else:
+                        self._memory.append(line)
                     self._memory.append(line.replace('\n', ''))
                 elif line[0] != ';':
+                    # removes comment from line
+                    line = line[:line.find(';')]
+                    if not line.isspace():
+                        self._memory.append(line)
                     self._memory.append(line[:line.find(';')])
         except FileNotFoundError:
             print(f'File: {file} not found')
@@ -104,7 +113,7 @@ class vm:
                 self._in(args[0])
             case 'OUT':
                 self._out(self.parse(line[line.find(' ') + 1:]))
-            case 'HALT' | 'HAL':
+            case 'HALT':
                 quit(0)
             # default case from unknown operation name
             case _:
